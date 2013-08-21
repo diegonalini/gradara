1f4a518b84aec773f0a2065557cd2a30c9d853e6
Diego Nalini <diegonalini@yahoo.com>
2013-08-21 21:49:52 +0200

    initial commit

---
+++ public/js/collections/TodoList.js
@@ -0,0 +1,32 @@
+/*global define */
+
+define([
+	'backbone',
+	'models/Todo',
+	'localStorage'
+], function (Backbone, Todo) {
+	'use strict';
+
+	function isCompleted(todo) {
+		return todo.get('completed');
+	}
+
+	return Backbone.Collection.extend({
+		model: Todo,
+
+		//localStorage: new Backbone.LocalStorage('todos-backbone'),
+		url: '/api/todos',
+
+		getCompleted: function () {
+			return this.filter(isCompleted);
+		},
+
+		getActive: function () {
+			return this.reject(isCompleted);
+		},
+
+		comparator: function (todo) {
+			return todo.get('created');
+		}
+	});
+});
---
+++ public/js/models/Todo.js
@@ -0,0 +1,31 @@
+/*global define */
+
+define([
+	'backbone',
+	'localStorage'
+], function (Backbone) {
+	'use strict';
+
+	return Backbone.Model.extend({
+		//localStorage: new Backbone.LocalStorage('todos-backbone'),
+		idAttribute: "_id",
+		
+		defaults: {
+			title: '',
+			completed: false,
+			created: 0
+		},
+
+		initialize: function () {
+			if (this.isNew()) {
+				this.set('created', Date.now());
+			}
+		},
+
+		toggle: function () {
+			//return this.set('completed', !this.get('completed'));
+			this.save({completed: !this.get("completed")});
+		}
+	});
+});
+
