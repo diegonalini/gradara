659908f34e8bb5f0821a5b656205c402d3362b7a
Diego Nalini <diegonalini@yahoo.com>
2013-08-28 08:26:32 +0200

    menu

--- public/js/templates.js
+++ public/js/templates.js
@@ -10,7 +10,6 @@ define(function (require) {
 		header: require('tpl!templates/todoHeader.tmpl'),
 		login: require('tpl!templates/login.tmpl'),
 		todo: require('tpl!templates/todo.tmpl'),
-		tableExample: require('tpl!templates/tableExample.tmpl'),
 		tableExG: require('tpl!templates/tableExG.tmpl'),
 		tableExR: require('tpl!templates/tableExR.tmpl')
 		
