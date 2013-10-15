82c3dbc20f7854c90da7467c4d11c891c4763d43
Diego Nalini <diegonalini@geocities.com>
2013-10-15 23:20:31 +0200

    mongo env uri2

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'json'
 require 'sinatra/security'
 
 url=ENV['OPENSHIFT_MONGODB_DB_URL']
-url='mongodb://127.0.0.1:27017/' if port==nil
+url='mongodb://127.0.0.1:27017/' if url==nil
 #host=ENV['OPENSHIFT_MONGODB_DB_HOST']
 #host='127.0.0.1' if host==nil
 DB = Mongo::Connection.from_uri( uri )
