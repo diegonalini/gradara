d08f25c1a51d3e6a1e38dac7889c72a227c8bab8
Diego Nalini <diegonalini@geocities.com>
2013-10-17 23:57:02 +0200

    ajax kill

--- app.rb
+++ app.rb
@@ -11,10 +11,18 @@ url=ENV['OPENSHIFT_MONGODB_DB_URL']
 use Rack::SslEnforcer   if (url!=nil) #, :only => %r{^/login/}
 
 
-before_filter :redirect_to_ssl
-def redirect_to_ssl
-    redirect_to :protocol => "https://" unless (request.ssl?)
-end
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    if env['HTTPS'] == 'on' || env['HTTP_X_FORWARDED_PROTO'] == 'https'
+      @app.call(env)
+    else
+      req = Rack::Request.new(env)
+      [301, { "Location" => req.url.gsub(/^http:/, "https:") }, []]
+    end
+  end
 
 url=ENV['OPENSHIFT_MONGODB_DB_URL']
 url='mongodb://127.0.0.1:27017/' if url==nil
